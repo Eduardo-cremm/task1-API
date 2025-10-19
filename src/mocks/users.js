40f23727f48aca8fd4738052212af6199ca5d6fb
Eduardo Cremm <duducremm@gmail.com>
2025-10-18 23:42:21 -0300

    First commit

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Rafael',
+    },
+    {
+        id: 2,
+        name: 'Eduardo',
+    },
+    {   
+        id: 3,
+        name: 'Charlie Brown',
+    },
+];
