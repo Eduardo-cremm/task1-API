8a65cfbfdbbc6febf594795fb4b5ce7bd511f543
Eduardo Cremm <duducremm@gmail.com>
2025-10-19 21:33:25 -0300

    arrumando identação

--- src/mocks/users.js
+++ src/mocks/users.js
@@ -1,14 +1,14 @@
 module.exports = [
-    {
-        id: 1,
-        name: 'Rafael',
-    },
-    {
-        id: 2,
-        name: 'Eduardo',
-    },
-    {   
-        id: 3,
-        name: 'Charlie Brown',
-    },
+  {
+    id: 1,
+    name: "Rafael",
+  },
+  {
+    id: 2,
+    name: "Eduardo",
+  },
+  {
+    id: 3,
+    name: "Charlie Brown",
+  },
 ];
